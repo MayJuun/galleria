7e7598392db7f695947ec201dfbb3dd5dc4fae00
Dokotela <grey.faulkenberry@mayjuun.com>
2022-11-18 21:11:25 -0500

    updating to make a dev branch

---
+++ scripts/identity.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -i -X POST https://doraemon-mctbmzb4uq-uc.a.run.app/create_user -d '{"email":"spitefuljellyfishtrifle@gmail.com","password":"8qhSBfPiuEHhfp7gq3L"}' -H 'Content-Type: application/json'
