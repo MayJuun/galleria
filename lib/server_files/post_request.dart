53539004d6ae89567a50e9550931cbe39fb82cfb
Dokotela <grey.faulkenberry@mayjuun.com>
2023-01-11 22:33:12 -0500

    For meld only need communicationRequest

--- lib/server_files/post_request.dart
+++ lib/server_files/post_request.dart
@@ -6,14 +6,14 @@ import '../galleria.dart';
 Future<Response> postRequest(List<String> path, [Resource? resource]) async {
   print('postRequest -- path: $path -- resource: $resource');
   switch (path[0]) {
-    case 'ServiceRequest':
-      return postRequestServiceRequest(path[1]);
-    case 'Task':
-      return postRequestTask(path[1]);
-    case 'Observation':
-      return postRequestObservation(path[1]);
-    case 'Condition':
-      return postRequestCondition(path[1]);
+    // case 'ServiceRequest':
+    //   return postRequestServiceRequest(path[1]);
+    // case 'Task':
+    //   return postRequestTask(path[1]);
+    // case 'Observation':
+    //   return postRequestObservation(path[1]);
+    // case 'Condition':
+    //   return postRequestCondition(path[1]);
     case 'CommunicationRequest':
       return postRequestCommunicationRequest(path[1]);
     default:
