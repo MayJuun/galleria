f4b6de894aeca729eef126f185ad0e8efcaf6bbf
Dokotela <grey.faulkenberry@mayjuun.com>
2023-03-01 11:26:32 -0500

    Updated send_via_email for communicationsOnly

--- lib/services/send_via_email.dart
+++ lib/services/send_via_email.dart
@@ -6,38 +6,43 @@ import 'package:shelf/shelf.dart';
 
 import '../galleria.dart';
 
+/// Function for sending a message via email
 Future<Response> sendViaEmail(String email, String text) async {
-  // if (email.contains('@mayjuun.com')) {
-  String _getBase64Email(String source) => base64UrlEncode(utf8.encode(source));
+  /// If it's communicationsOnly, we're assuming we allow any email to be
+  /// entered. Otherwise, for now, we're only allowing MayJuun emails
+  if (clientAssets.communicationsOnly || email.contains('@mayjuun.com')) {
+    String _getBase64Email(String source) =>
+        base64UrlEncode(utf8.encode(source));
 
-  final clientCredentials = emailAccountCredentials;
+    final clientCredentials = emailAccountCredentials;
 
-  final authClient = await clientViaServiceAccount(
-      clientCredentials, ['https://www.googleapis.com/auth/gmail.send']);
+    final authClient = await clientViaServiceAccount(
+        clientCredentials, ['https://www.googleapis.com/auth/gmail.send']);
 
-  final gmailApi = gMail.GmailApi(authClient);
+    final gmailApi = gMail.GmailApi(authClient);
 
-  String from = 'service.account@mayjuun.com';
-  String to = email;
-  String subject = 'Message from MayJuun';
-  String contentType = 'text/html';
-  String charset = 'utf-8';
-  String contentTransferEncoding = 'base64';
-  String emailContent = text;
+    String from = 'service.account@mayjuun.com';
+    String to = email;
+    String subject = 'Message from MayJuun';
+    String contentType = 'text/html';
+    String charset = 'utf-8';
+    String contentTransferEncoding = 'base64';
+    String emailContent = text;
 
-  await gmailApi.users.messages.send(
-      gMail.Message.fromJson({
-        'raw': _getBase64Email('From: $from\r\n'
-            'To: $to\r\n'
-            'Subject: $subject\r\n'
-            'Content-Type: $contentType; charset=$charset\r\n'
-            'Content-Transfer-Encoding: $contentTransferEncoding\r\n\r\n'
-            '$emailContent'),
-      }),
-      from);
+    await gmailApi.users.messages.send(
+        gMail.Message.fromJson({
+          'raw': _getBase64Email('From: $from\r\n'
+              'To: $to\r\n'
+              'Subject: $subject\r\n'
+              'Content-Type: $contentType; charset=$charset\r\n'
+              'Content-Transfer-Encoding: $contentTransferEncoding\r\n\r\n'
+              '$emailContent'),
+        }),
+        from);
 
-  return Response.ok('Message has been sent: ${DateTime.now()}');
-  // } else {
-  //   return Response.ok('$email is not within the MayJuun Domain');
-  // }
+    return Response.ok('Message has been sent: ${DateTime.now()}');
+  } else {
+    /// If it's not a MayJuun email, we return a response to that effect
+    return Response.ok('$email is not within the MayJuun Domain');
+  }
 }
